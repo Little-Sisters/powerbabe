fcade467644b2055e85f2f29934fa97343594c6f
mims <113519976+MimPMC@users.noreply.github.com>
2024-02-01 23:59:57 +0100

    images  src started

---
+++ client/public/data/bodyData.ts
@@ -0,0 +1,26 @@
+
+type LowerBodyImages = {
+  medium: string[];
+  tan: string[];
+};
+
+const lowerBodyArray: LowerBodyImages = {
+  medium: [
+    "./characters/lowerbody-1-medium.png",
+    "./characters/lowerbody-2-medium.png",
+  ],
+  tan: [
+    // Add tan image paths here if applicable
+  ],
+};
+
+const upperBodyArray = [
+  "./characters/upperbody-1-medium.png",
+  "./characters/upperbody-2-medium.png",
+];
+
+const topArray = [
+
+  "./characters/top-1-black.png",
+  "./characters/top-2-black.png",
+];
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import styled from "styled-components";
 
 interface SpriteProps {
@@ -8,61 +7,53 @@ interface SpriteProps {
 }
 
 function App() {
-  const [hairIndex, setHairIndex] = useState(0);
-  const [topIndex, setTopIndex] = useState(0);
-
-  const hairArray = [
-    "./characters/hair-light-1.png",
-    "./characters/hair-2-dark.png",
-    // Add more hair images as needed
-  ];
-
-  const topArray = [
-    "./characters/top-1-black.png",
-    "./characters/top-2-black.png",
-    // Add more top images as needed
-  ];
-
-  const changeHair = () => {
-    console.log("changing hair");
-    setHairIndex((prevIndex) => (prevIndex + 1) % hairArray.length);
-  };
-
-  const changeTop = () => {
-    console.log("changing top");
-    setTopIndex((prevIndex) => (prevIndex + 1) % topArray.length);
-  };
-
   return (
     <div className="App">
       <h1>Powerbabe the game version 0</h1>
       <Flex>
-        <button onClick={changeHair}>Change Hair</button>
-        <button onClick={changeTop}>Change Top</button>
+        <ChooseBox>
+          <button>previous</button>
+          <button>next one</button>
+        </ChooseBox>
       </Flex>
-      <CharachterContainer>
-        <Sprite src={topArray[topIndex]} zindex={25}></Sprite>
-        <Sprite src="./characters/bottoms-1-blue.png" zindex={20}></Sprite>
-        <Sprite src="./characters/eyes-1-blue.png" zindex={3}></Sprite>
-        <Sprite src="./characters/eyebrows-light-1.png" zindex={3}></Sprite>
-        <Sprite src="./characters/lips-1-pink.png" zindex={3}></Sprite>
-        <Sprite src="./characters/head-1.png" zindex={2}></Sprite>
-        <Sprite src="./characters/lowerbody-light-1.png" zindex={10}></Sprite>
-        <Sprite src={hairArray[hairIndex]} zindex={5}></Sprite>
-        <Sprite src="./characters/upperbody-light-1.png" zindex={10}></Sprite>
-      </CharachterContainer>
+      <Flex>
+        <CharachterContainer>
+          <Sprite src="./characters/head-medium.png"></Sprite>
+          <Sprite
+            src="./characters/lowerbody-2-medium.png"
+            zindex={10}
+          ></Sprite>
+          <Sprite
+            src="./characters/upperbody-1-medium.png"
+            zindex={10}
+          ></Sprite>
+        </CharachterContainer>
+      </Flex>
     </div>
   );
 }
 
 const Flex = styled.div`
   display: flex;
+  width: 100%;
+  background: beige;
+  justify-content: center;
+  align-items: center;
+
+`;
+
+const ChooseBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 20rem;
+  height: 7rem;
+  background: hotpink;
 `;
 
 const CharachterContainer = styled.div`
   height: 40rem;
-  background: #ffffff;
-  width: 100%;
+  background: #ffa6f3;
+  width: 20rem;
   position: relative;
 `;
 
