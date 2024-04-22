e179d11ab858034b08ac449d083931c7289ff46e
mims <113519976+MimPMC@users.noreply.github.com>
2024-04-22 21:30:37 +0200

    more stuffs

--- client/src/assets/data.ts
+++ client/src/assets/data.ts
@@ -19,6 +19,7 @@ export const hairstyleData: StylesAndColorsData = {
       colors: [
         "platinum",
         "blonde",
+        "caramel",
         "brown",
         "black",
         "ginger",
@@ -34,6 +35,7 @@ export const hairstyleData: StylesAndColorsData = {
       colors: [
         "platinum",
         "blonde",
+        "caramel",
         "brown",
         "black",
         "ginger",
@@ -49,6 +51,7 @@ export const hairstyleData: StylesAndColorsData = {
       colors: [
         "platinum",
         "blonde",
+        "caramel",
         "brown",
         "black",
         "ginger",
@@ -64,6 +67,7 @@ export const hairstyleData: StylesAndColorsData = {
       colors: [
         "platinum",
         "blonde",
+        "caramel",
         "brown",
         "black",
         "ginger",
@@ -79,6 +83,7 @@ export const hairstyleData: StylesAndColorsData = {
       colors: [
         "platinum",
         "blonde",
+        "caramel",
         "brown",
         "black",
         "ginger",
@@ -95,6 +100,7 @@ export const hairstyleData: StylesAndColorsData = {
       colors: [
         "platinum",
         "blonde",
+        "caramel",
         "brown",
         "black",
         "ginger",
